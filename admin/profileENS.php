7132e25dc3f2a90d677a839479d8256f8c059ebe
mehdi <mehdizse@gmail.com>
2018-03-30 23:24:33 +0300

    ajout du profil et recuperation des informations de la base de données

--- admin/profileENS.php
+++ admin/profileENS.php
@@ -6,11 +6,11 @@
 
 $bdd = new PDO('mysql:host=127.0.0.1;dbname=test2;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-
-$id = $bdd->prepare('SELECT * FROM ensegniants');
+$compte=(int) $_GET['id_ens'];
+$id = $bdd->prepare("SELECT * FROM ensegniants where id=".$compte);
 $id->execute();
 
-
+$compt=$id->fetch();
 
 
 /* Supprimer un medecin de la base de donnee*/
@@ -162,7 +162,7 @@ if(isset($_POST['update'])){
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">Menu Principale</li>
-       <li><a href="index.php"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
+       <li><a href="index.html"><i class="fa fa-book"></i> <span>Dashboard</span></a></li>
         <li><a href="Enseignants.php"><i class="fa fa-book"></i> <span>Enseignants</span></a></li>
         <li><a href="Etudiants.php"><i class="fa fa-book"></i> <span>Etudiants</span></a></li>
         <li><a href="Themes.php"><i class="fa fa-book"></i> <span>Themes</span></a></li>
@@ -181,39 +181,134 @@ if(isset($_POST['update'])){
       </h1>
     </section>
 
+
     <!-- Main content -->
     <section class="content">
-      <table id="example1" class="table table-bordered table-striped">
-        <thead>
-        <tr>
-          <th>ID</th>
-          <th>CODE</th>
-          <th>Nom</th>
-          <th>Prenom</th>
-          <th>Grade</th>
-          <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php while($m=$id->fetch()) { ?>
-          <tr>
-        <td>  <?=$m['id']?>  </td>
-        <td>  <?=$m['code']?>  </td>
-        <td>  <?=$m['nom']?>  </td>
-        <td>  <?=$m['prenom']?>  </td>
-        <td>  <?=$m['grade']?>  </td>
-        <td>
+
+      <div class="row">
+        <div class="col-md-3">
+
+          <!-- Profile Image -->
+          <div class="box box-primary">
+            <div class="box-body box-profile">
+              <img class="profile-user-img img-responsive img-circle" src="dist/img/user4-128x128.jpg" alt="User profile picture">
+
+              <h3 class="profile-username text-center"><?=$compt['nom']?> <?=$compt['prenom']?> </h3>
+
+              <p class="text-muted text-center"><?=$compt['grade']?></p>
 
 
-      <button class="btn btn-danger btn-sm" onclick="deleteme(<?php echo $m['id'] ?>)" > Supprimer</button>
-      <a href="profileEns.php?&id_ens=<?= $m['id'] ?>">
-      <button class="btn btn-default btn-sm" ><i class="fa fa-ey "></i> Détail</button></a>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
 
-      </td>
-        </tr>
-      <?php }?>
-        </tbody>
-      </table>
+          <!-- About Me Box -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">A propos de l'enseignant</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <strong><i class="fa fa-calendar margin-r-5"></i> DateNaissance</strong>
+
+              <p class="text-muted">
+              <?=$compt['datNai']?>
+              </p>
+
+              <strong><i class="fa fa-map-marker margin-r-5"></i> Addresse</strong>
+
+              <p class="text-muted"><?=$compt['adress']?></p>
+
+              <strong><i class="fa fa-phone margin-r-5"></i> Numero Telephone</strong>
+
+              <p class="text-muted"><?=$compt['tlf']?></p>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-9">
+          <div class="nav-tabs-custom">
+            <ul class="nav nav-tabs">
+              <li class="active"><a href="#activity" data-toggle="tab">EtudiantsEncadre</a></li>
+              <li><a href="#timeline" data-toggle="tab">ThemesPropose</a></li>
+              <li><a href="#settings" data-toggle="tab">Modifier</a></li>
+            </ul>
+            <div class="tab-content">
+              <div class="active tab-pane" id="activity">
+
+
+              </div>
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="timeline">
+
+              </div>
+              <!-- /.tab-pane -->
+
+              <div class="tab-pane" id="settings">
+                <form class="form-horizontal">
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                    <div class="col-sm-10">
+                      <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="inputName" placeholder="Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                    <div class="col-sm-10">
+                      <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="inputSkills" class="col-sm-2 control-label">Skills</label>
+
+                    <div class="col-sm-10">
+                      <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <div class="checkbox">
+                        <label>
+                          <input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-2 col-sm-10">
+                      <button type="submit" class="btn btn-danger">Submit</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <!-- /.tab-pane -->
+            </div>
+            <!-- /.tab-content -->
+          </div>
+          <!-- /.nav-tabs-custom -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
 
     </section>
     <!-- /.content -->
@@ -228,50 +323,50 @@ if(isset($_POST['update'])){
   </footer>
 
 
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
+  <!-- jQuery 3 -->
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
   $.widget.bridge('uibutton', $.ui.button);
-</script>
-<!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="bower_components/moment/min/moment.min.js"></script>
-<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
-<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-<script src="dist/js/sweetalert-dev.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-<script>
+  </script>
+  <!-- Bootstrap 3.3.7 -->
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- Morris.js charts -->
+  <script src="bower_components/raphael/raphael.min.js"></script>
+  <script src="bower_components/morris.js/morris.min.js"></script>
+  <!-- Sparkline -->
+  <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+  <!-- jvectormap -->
+  <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+  <!-- daterangepicker -->
+  <script src="bower_components/moment/min/moment.min.js"></script>
+  <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+  <!-- datepicker -->
+  <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+  <!-- Slimscroll -->
+  <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="bower_components/fastclick/lib/fastclick.js"></script>
+  <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+  <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="dist/js/pages/dashboard.js"></script>
+  <script src="dist/js/sweetalert-dev.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+  <script>
   $(function () {
     $('#example1').DataTable({
       'iDisplayLength': 5,
@@ -310,7 +405,7 @@ if(isset($_POST['update'])){
              }
           });
         }
-</script>
-</script>
-</body>
-</html>
+  </script>
+  </script>
+  </body>
+  </html>
