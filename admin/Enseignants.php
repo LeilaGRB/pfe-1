30498ed169129dcaca9b336c0271d9ff56cb14ef
mehdi <mehdizse@gmail.com>
2018-03-30 19:56:03 +0300

    ajout des fichiers style pour suppresion et recuperation des donnes de la table enseignant

--- admin/Enseignants.php
+++ admin/Enseignants.php
@@ -1,3 +1,51 @@
+
+<?php
+
+
+
+
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=test2;charset=utf8', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+
+$id = $bdd->prepare('SELECT * FROM ensegniants');
+$id->execute();
+
+
+
+
+/* Supprimer un medecin de la base de donnee*/
+
+if(isset($_GET['supprimer_ens'])){
+    $id_ens = (int) $_GET['supprimer_ens'];
+    $id = $bdd->prepare("SELECT compt_id FROM ensegniants WHERE id=".$id_ens);
+    $id->execute();
+    $compte=$id->fetch();
+    $supp = $bdd->prepare("DELETE FROM comptes WHERE id=".$compte['compt_id']);
+    $supp->execute();
+     $suppr = $bdd->prepare("DELETE FROM ensegniants WHERE id=".$id_ens);
+     $suppr->execute();
+    header('Location: Enseignants.php');
+}
+
+/*Requete mise a jour bdd*/
+if(isset($_POST['update'])){
+  $id_med=$_POST['id_med'];
+  $nom=$_POST['nom'];
+  $prenom=$_POST['prenom'];
+  $dn=$_POST['datepicker'];
+  $adress=$_POST['adress'];
+  $N_tel=$_POST['N_tel'];
+  $grade=$_POST['grade'];
+  $specialite=$_POST['specialite'];
+
+   $exu = $auth_user->runQuery("UPDATE medecin set nom='".$nom."',prenom='".$prenom."',DN='".$dn."',adresse='".$adress."',N_tel='".$N_tel."',grade='".$grade."',specialite='".$specialite."' where id_med='".$id_med."'");
+   $exu->execute();
+   header('location:medecins.php');
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +58,8 @@
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
@@ -27,6 +77,8 @@
   <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <link rel="stylesheet" type="text/css" href="dist/css/sweetalert.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -131,70 +183,38 @@
 
     <!-- Main content -->
     <section class="content">
-      <!-- Small boxes (Stat box) -->
-      <div class="row">
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-aqua">
-            <div class="inner">
-              <h3>5</h3>
+      <table id="example1" class="table table-bordered table-striped">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>CODE</th>
+          <th>Nom</th>
+          <th>Prenom</th>
+          <th>Grade</th>
+          <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php while($m=$id->fetch()) { ?>
+          <tr>
+        <td>  <?=$m['id']?>  </td>
+        <td>  <?=$m['code']?>  </td>
+        <td>  <?=$m['nom']?>  </td>
+        <td>  <?=$m['prenom']?>  </td>
+        <td>  <?=$m['grade']?>  </td>
+        <td>
 
-              <p>Enseignants</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="Enseignants.php" class="small-box-footer">Détails <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-green">
-            <div class="inner">
-              <h3>10</h3>
 
-              <p>Etudiants</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-person-add"></i>
-            </div>
-            <a href="Etudiants.php" class="small-box-footer">Détails <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-yellow">
-            <div class="inner">
-              <h3>8</h3>
+      <button class="btn btn-danger btn-sm" onclick="deleteme(<?php echo $m['id'] ?>)" > Supprimer</button>
+      <a href="info_infirmier.php?&id_inf=<?= $m['id_inf'] ?>">
+      <button class="btn btn-default btn-sm" ><i class="fa fa-ey "></i> Détail</button></a>
+      <input type="button" name="edit" value="Modifier" id="<?php echo $m['id_inf']; ?>" class="btn btn-warning btn-sm edit_data" />
 
-              <p>Thémes</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="Themes.php" class="small-box-footer">Détails <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box bg-red">
-            <div class="inner">
-              <h3>65</h3>
-
-              <p>FICHE DE VOEUX</p>
-            </div>
-            <div class="icon">
-              <i class="ion ion-bag"></i>
-            </div>
-            <a href="Voeux.php" class="small-box-footer">Détails <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
-
+      </td>
+        </tr>
+      <?php }?>
+        </tbody>
+      </table>
 
     </section>
     <!-- /.content -->
@@ -243,11 +263,55 @@
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="dist/js/pages/dashboard.js"></script>
+<script src="dist/js/sweetalert-dev.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
+<script>
+  $(function () {
+    $('#example1').DataTable({
+      'iDisplayLength': 5,
+      'aLengthMenu': [[5, 10, 100, -1], [5, 10, "TOUT"]],
+      "oLanguage": {
+          "sSearch": "Rechercher: :",
+          "oPaginate": { "sNext": "Suivant" ,"sPrevious": "Precedent" },
+          "sEmptyTable": "Table Vide",
+          "sInfo": "",
+          "sLengthMenu": "Afficher _MENU_ Ligne",
+          "sZeroRecords": "Aucun Resultat Trouve",
+          "sInfoFiltered": "",
+          "sInfoEmpty": ""
+      }
+    })
+  })
+  function deleteme (delid)
+     {
+       swal({
+        title: "Êtes-vous sûr?",
+        text: "Vous ne pourrez pas récupérer ces informations",
+          type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Oui, supprimez !",
+            cancelButtonText: "Non, annulez!",
+            closeOnConfirm: false,
+            closeOnCancel: false
+             },
+            function(isConfirm){
+            if (isConfirm) {
+          window.location.href='?supprimer_ens='+delid+'';
+          swal("Supprimé!", "infirmier supprimé .", "success");
+           } else {
+             swal("Annulé", "infirmier non supprime :)", "error");
+             }
+          });
+        }
+</script>
+</script>
 </body>
 </html>
